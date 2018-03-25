0a4d13a7f8f6af198401424e4e25c66f3a91cff6
vincentleandr <31660411+vincentleandr@users.noreply.github.com>
2018-03-25 22:05:18 +0700

    .

--- src/components/display.js
+++ src/components/display.js
@@ -2,12 +2,34 @@ import React from 'react';
 import Parallax from 'parallax-js';
 
 class Display extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            convText: ''
+        }
+    }
+
     componentDidMount() {
-        this.parallax = new Parallax(this.refs.poster)
+        this.parallax = new Parallax(this.refs.poster);
+    }
+
+    componentDidUpdate() {
+        let summaryText = this.props.summary;
+        this.summaryText = summaryText.replace(/<p>|<\/p>|<b>|<\/b>|<i>|<\/i>/g, "");
+
+        if(this.summaryText !== this.state.convText) {
+            this.setState({
+                convText: this.summaryText
+            });
+        }
+
+        
+        
     }
     
     componentWillUnmount() {
-        this.parallax.disable()
+        this.parallax.disable();
     }
 
     render() {
@@ -20,14 +42,15 @@ class Display extends React.Component {
                 
                 <div className="shows-details">
                     <h1 className="shows-title">{this.props.name}</h1>
-                    <div className="shows-summary">{this.props.summary}</div>
+                    <div className="shows-summary">{this.state.convText}</div>
                 </div>
-                
                 
             </div>
             
         );
     }
+
+
 }
 
 export default Display;
